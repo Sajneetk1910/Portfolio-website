ee01d3bd7483fa0229e2d18b731f781b79e5471b
Sajneetk1910 <your-email@example.com>
2025-04-04 07:08:18 +0530

    skills seection

---
+++ index.js
@@ -0,0 +1,21 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//     const darkModeBtn = document.getElementById("darkModeBtn");
+//     const body = document.body;
+  
+//     // Check local storage for dark mode preference
+//     if (localStorage.getItem("darkMode") === "enabled") {
+//       body.classList.add("dark-mode");
+//     }
+  
+//     darkModeBtn.addEventListener("click", () => {
+//       body.classList.toggle("dark-mode");
+  
+//       // Save preference in local storage
+//       if (body.classList.contains("dark-mode")) {
+//         localStorage.setItem("darkMode", "enabled");
+//       } else {
+//         localStorage.setItem("darkMode", "disabled");
+//       }
+//     });
+//   });
+  
