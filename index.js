44c292a80662d61b648afaafbe6d471c873d4987
Sajneetk1910 <your-email@example.com>
2025-04-08 11:43:54 +0530

    homepage completed

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//     const darkModeBtn = document.getElementById("darkModeBtn");
-//     const body = document.body;
+document.addEventListener("DOMContentLoaded", () => {
+    const darkModeBtn = document.getElementById("darkModeBtn");
+    const body = document.body;
   
-//     // Check local storage for dark mode preference
-//     if (localStorage.getItem("darkMode") === "enabled") {
-//       body.classList.add("dark-mode");
-//     }
+    // Check local storage for dark mode preference
+    if (localStorage.getItem("darkMode") === "enabled") {
+      body.classList.add("dark-mode");
+    }
   
-//     darkModeBtn.addEventListener("click", () => {
-//       body.classList.toggle("dark-mode");
+    darkModeBtn.addEventListener("click", () => {
+      body.classList.toggle("dark-mode");
   
-//       // Save preference in local storage
-//       if (body.classList.contains("dark-mode")) {
-//         localStorage.setItem("darkMode", "enabled");
-//       } else {
-//         localStorage.setItem("darkMode", "disabled");
-//       }
-//     });
-//   });
+      // Save preference in local storage
+      if (body.classList.contains("dark-mode")) {
+        localStorage.setItem("darkMode", "enabled");
+      } else {
+        localStorage.setItem("darkMode", "disabled");
+      }
+    });
+  });
   
 
 // Set current year dynamically
@@ -25,4 +25,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const yearSpan = document.getElementById("year");
     yearSpan.textContent = new Date().getFullYear();
   });
+
+
   
